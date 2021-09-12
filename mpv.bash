8eea40374c00df111e42a5619a4a91ccc15d34e2
mjclinic <20180817@vision.hoseo.edu>
2021-09-12 19:00:01 +0900

    e

--- mpv.bash
+++ mpv.bash
@@ -4,4 +4,5 @@ curl "https://dic.daum.net/search.do?q=pattern" |\
   sed -En -e '/tit_word"/{s/(<[^>]+>)(.+)(<[^>]+>)/\2/;bX}'\
   -e '/"num_search"/{s/>([^<]+)|(.)/\1/g;bX}'\
           -e 'b'\
-          -e ':X p '
+          -e ':X p'\
+          -e 'e sleep 1'
