922a54ebb960e58a2178766fcdf81ec22c31fb60
mjclinic <20180817@vision.hoseo.edu>
2021-09-20 21:49:07 +0900

    sed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed excutesed exct

--- mpv.bash
+++ mpv.bash
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 curl "https://dic.daum.net/search.do?q=pattern" |\
-  sed -En -e '/tit_word"/{s/(<[^>]+>)(.+)(<[^>]+>)/\2/;bX}'\
-  -e '/"num_search"/{s/>([^<]+)|(.)/\1/g;bX}'\
+  sed -En -e '/tit_word"/{s/(.+)(<[^>]+>)(.+)(<[^>]+>)/\3/;bX}'\
+  -e '/"num_search"/{s/>([^<]+)|(.)/\1/g;s/^\s*//;bX}'\
+  -e '/txt_searchword/{s/<[^>]+>//g;s/^\s*//;bX}'\
+  -e '/data-audio data-url=/{s/(.+)url=("[^"]+")(.+)/mpv \2 >/dev/null;/e;bX}'\
           -e 'b'\
           -e ':X p'\
-          -e 'e sleep 1'
+          
